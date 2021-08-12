7402c5c4eb853be33aeeaad6240e1e92840aca75
Gerardo <gerardo@latteandcode.com>
2021-08-12 15:40:34 +0200

    first commit

--- README.md
+++ README.md
@@ -1,46 +1,13 @@
-# Getting Started with Create React App
+# ⛩ Create a tour by the application with React
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Description
 
-## Available Scripts
+This is the repository associated to my video https://youtu.be/8b_6XvXuj8s in which I explain how we can create a tour of the application using the **Joyride** library.
 
-In the project directory, you can run:
+What we will do is create a custom hook called `useTour` that will allow us to obtain a component that will paint a tour through a view of our application, showing a "popup" in each of the elements that we indicated.
 
-### `yarn start`
+In addition, this video will allow us to continue seeing new use cases for hooks, so it is also very useful in that sense. Specifically, we will review the `useEffect`, `useState` and `useMemo` hooks.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Run the project
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+Use yarn start to run the app in the development mode and open http://localhost:3000 to view it in the browser.
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useTour from "./useTour";
+import { Step } from "react-joyride";
+
+const STEPS: Step[] = [
+  {
+    content: <h2>Let's begin our journey!</h2>,
+    locale: { skip: <strong aria-label="skip">S-K-I-P</strong> },
+    placement: "center",
+    target: "body",
+  },
+  {
+    content: <h2>Sticky elements</h2>,
+    floaterProps: {
+      disableAnimation: true,
+    },
+    spotlightPadding: 20,
+    target: ".demo__hero h2",
+  },
+  {
+    content: "These are our super awesome projects!",
+    placement: "bottom",
+    styles: {
+      options: {
+        width: 300,
+      },
+    },
+    target: ".demo__projects h2",
+    title: "Our projects",
+  },
+  {
+    content: (
+      <div>
+        You can render anything!
+        <br />
+        <h3>Like this H3 title</h3>
+      </div>
+    ),
+    placement: "top",
+    target: ".demo__how-it-works h2",
+    title: "Our Mission",
+  },
+  {
+    content: (
+      <div>
+        <h3>All about us</h3>
+        <svg
+          width="50px"
+          height="50px"
+          viewBox="0 0 96 96"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="xMidYMid"
+        >
+          <g>
+            <path
+              d="M83.2922435,72.3864207 C69.5357835,69.2103145 56.7313553,66.4262214 62.9315626,54.7138297 C81.812194,19.0646376 67.93573,0 48.0030634,0 C27.6743835,0 14.1459311,19.796662 33.0745641,54.7138297 C39.4627778,66.4942237 26.1743334,69.2783168 12.7138832,72.3864207 C0.421472164,75.2265157 -0.0385432192,81.3307198 0.0014581185,92.0030767 L0.0174586536,96.0032105 L95.9806678,96.0032105 L95.9966684,92.1270809 C96.04467,81.3747213 95.628656,75.2385161 83.2922435,72.3864207 Z"
+              fill="#000000"
+            />
+          </g>
+        </svg>
+      </div>
+    ),
+    placement: "left",
+    target: ".demo__about h2",
+  },
+  {
+    content: (
+      <div>
+        You can render anything!
+        <br />
+        <h3>Like this H3 title</h3>
+      </div>
+    ),
+    placement: "top",
+    target: ".demo__about__text",
+    title: "Our Mission",
+  },
+];
 
 function App() {
+  const tour = useTour(STEPS, "LS_KEY");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="demo-wrapper">
+      {tour}
+      <div className="demo__section demo__hero">
+        <div>
+          <h2>Create guided tours for your apps</h2>
+        </div>
+      </div>
+      <div className="demo__section demo__projects">
+        <h2>OUR PROJECTS</h2>
+      </div>
+      <div className="demo__section demo__how-it-works">
+        <h2>HOW DOES IT WORK</h2>
+      </div>
+      <div className="demo__section demo__about">
+        <h2>ABOUT US</h2>
+        <div className="demo__about__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+      </div>
     </div>
   );
 }
